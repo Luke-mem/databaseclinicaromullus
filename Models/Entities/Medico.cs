132359c792f06131c63a5afd5848f72d98d8f313
Aluno <aluno@edu.senac.br>
2023-11-16 20:41:25 -0300

    clinica

---
+++ Models/Entities/Medico.cs
@@ -0,0 +1,16 @@
+public class Medico{ //início
+
+/*
+variável: é um espaço reservado na memória do computador 
+*/
+//POO é programação orientado a objetos
+//Na POO a variável é chamada também de atributo
+//No C# ela pode ser uma propriedade automática
+
+int Id;
+string? CRM;  //forma 1 de contornar (?) string não pode ser nulo
+string Nome = string.Empty; //forma 2 de contornar (string.Empty) string não pode ser nulo
+string Nome = ""; //forma 2 de contornar ("") string não pode ser nulo
+
+
+} //Fim
